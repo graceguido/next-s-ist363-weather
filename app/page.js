b64b169dd28fac97ae414089be3d407be9ff474e
Grace Guido <graceguido@Graces-MacBook-Pro.local>
2024-04-09 10:00:37 -0400

    environment variable added

--- app/page.js
+++ app/page.js
@@ -4,28 +4,56 @@ import { useState, useEffect } from "react";
 
 import Image from "next/image";
 
-import ButtonDemo from "@/components/ButtonDemo";
+import ButtonDemo from "../components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker";
 
-import { getPeople, getWeatherData } from "../lib/api";
+import {
+  getGeoLocation,
+  getPeople,
+  getWeatherData,
+  getWeatherDataByLatLon,
+} from "../lib/api";
 
 const Homepage = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   const peopleArr = getPeople();
 
   useEffect(() => {
+    getGeoLocation()
+      .then((position) => {
+        console.log(position);
+        setLocation(position);
+      })
+      .catch((error) => {
+        setErrorMsg(error);
+      });
+  }, []);
+
+  useEffect(() => {
     const fetchData = async () => {
-      const response = await getWeatherData();
+      const response = await getWeatherDataByLatLon(location);
       setWeatherData(response);
     };
-    fetchData();
-  }, [weatherData]);
-  // console.log({ peopleArr });
+    location ? fetchData() : null;
+  }, [location]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await getWeatherData();
+  //     setWeatherData(response);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  //console.log({ peopleArr });
   return (
     <div>
       <h1>Weather app</h1>
+      {errorMsg && <div>{errorMsg}</div>}
       {weatherData && (
         <div>
           <h2>{weatherData.city.name}</h2>
@@ -33,7 +61,7 @@ const Homepage = () => {
           <p>{weatherData.list[0].weather[0].description}</p>
           <Image
             src={`https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`}
-            alt={"Weather Icon"}
+            alt={`Weather icon for ${weatherData.list[0].weather[0].description}`}
             width={100}
             height={100}
           />
